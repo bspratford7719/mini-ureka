95192da474e09d0dcd96e38bbd95636b16f1c607
brittany spratford <britsprat@gmail.com>
2022-05-06 23:10:04 -0700

    updates to routes

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const uuid = require('uuid');
+
+
+
+
+module.exports  = router;
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const PORT = process.env.PORT || 3001;
+
 //const path = require("path");
-const app = express();
 const express = require("express");
-const uuid = require('uuid');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
