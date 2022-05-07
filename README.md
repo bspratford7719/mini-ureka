24de8b2a5bafca3f7f65e8254f9069ffe5d07a5c
brittany spratford <britsprat@gmail.com>
2022-05-06 21:52:39 -0700

    updates to db & server

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ I WANT to be able to write and save notes
 SO THAT I can organize my thoughts and keep track of tasks I need to complete
 
 # Links:
+- github:
+- heroku:
 
 # Screenshot:
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 const express = require("express");
 const uuid = require('uuid');
 
+app.use(express.static('public'));
 
-
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
